5ab37d2fe99a4e1f4f657e0db8d99919ac31aa5c
FullTimeScam <418.gingin@gmail.com>
2024-05-17 15:51:28 +0900

    배너 완성

--- src/components/SlideBanner.jsx
+++ src/components/SlideBanner.jsx
@@ -3,11 +3,15 @@ import Image1 from "/images/home_banner_1024_unity_1st.webp";
 const SlideBanner = () => {
   return (
     <div className="relative h-[400px]">
-      <div className="text-white">
+      <div className="container-style flex justify-center flex-col h-full text-white">
         <h3 className="text-[32px] font-bold">유니티 게임스쿨 1기</h3>
         <h5 className="mt-2">게임 개발자로 거듭나기 위한 6개월간의 여정</h5>
       </div>
-      <img className="absolute top-0 -z-10" src={Image1} alt="이세계 유니티" />
+      <img
+        className="absolute top-0 -z-10 w-full h-full object-cover"
+        src={Image1}
+        alt="이세계 유니티"
+      />
       {/*//z축으로 한 칸 뒤로 보내기*/}
     </div>
   );
